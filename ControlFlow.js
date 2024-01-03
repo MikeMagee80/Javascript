dde3d9ae02ea0513b13f65abd35b40577d05938f
MikeMagee80 <73151543+MikeMagee80@users.noreply.github.com>
2024-01-03 08:17:41 -0600

    Update ControlFlow.js

--- ControlFlow.js
+++ ControlFlow.js
@@ -84,3 +84,121 @@
 // ===================================================
 
 // DO....while
+
+// let i = 0;
+// do {
+//     if ( i % 2 != 0) console.log(i);
+//     i++;
+// } while (i <= 5);  // Do-While loops are always executed at least once
+
+//=========================================
+
+// For...in
+
+// const person = {
+//     name: 'Mike',
+//     age: 30
+// };
+
+// for (let key in person)
+//     console.log(key, person[key]);
+
+// // dot notation
+// person.name
+
+// // bracket notation
+// person['name']  bracket notation when we don't know the details
+
+// const colors = ['red', 'green', 'blue'];
+
+// for (let index in colors)
+//     console.log(index, colors[index]);
+
+//==============================================
+
+//Break and continue
+
+// let i = 0;
+// while (i <= 10) {
+//     // if (i === 5) break
+//     if (i % 2 === 0) {
+//         i++;
+//         continue;
+//     }
+
+//     console.log(i);
+//     i++;
+// }
+
+
+//===============================
+
+//Exercise: Write a function that takes two numbers and returns the maximum of the two.
+
+// let numberTest0 = maxNumber(3, 3);
+// console.log(numberTest0);
+
+// let numberTest1 = maxNumber(1, 2);
+// console.log(numberTest1);
+
+// let numberTest2 = maxNumber(5, 8);
+// console.log(numberTest2);
+
+// function maxNumber (number1, number2) {
+//     // if (number1 > number2) return number1;
+//     // return number2;
+
+//     return (number1 > number2) ? number1 : number2; //cleaner version
+
+// }
+
+// =====================================
+
+// Exercise: Landscape = width > height
+
+// let testCase = isLandscape(24, 12);
+// console.log(testCase);
+
+// let testCase2 = isLandscape(11, 34);
+// console.log(testCase2);
+
+// function isLandscape(width, height) {
+//     // if (width > height) return true;
+//     // return false;
+
+//     return (width > height);
+// }
+
+// ============================================
+
+// Exercise: Fizzbuzz
+
+
+// Divisible by 3 => Fizz
+// Divisible by 5 => Buzz
+// Divisible by both 3 and 5 => FizzBuzz!
+// Not divisible by 3 or 5 => input
+// Not a number => 'Not a number'
+
+// const output = fizzBuzz(15);
+// console.log(output);
+
+// function fizzBuzz(input) {
+//     if (typeof input !== 'number')
+//         return 'Not a number';
+
+//     if ((input % 3 === 0) && (input % 5 === 0)) // has to go first, or even qualifying inputs will return Fizz, due to order of operations
+//         return 'FizzBuzz!';    
+    
+//     if (input % 3 === 0) 
+//         return 'Fizz';
+    
+//     if (input % 5 === 0) 
+//         return 'Buzz';
+    
+
+    
+//     else return input;
+// }
+
+// ============================
