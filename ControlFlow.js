23cd81ceb6f584fe976e4afc4b756d9aa147f408
MikeMagee80 <73151543+MikeMagee80@users.noreply.github.com>
2024-01-04 13:48:53 -0600

    Update ControlFlow.js

--- ControlFlow.js
+++ ControlFlow.js
@@ -289,4 +289,57 @@
 // }
 
 
-//
+//  =================================================
+
+// Exercise
+// Sum of multiples of 3 and 5
+
+// console.log(sum(10));
+
+// function sum(limit) {
+//     let sum = 0;
+
+//     for (let i = 0; i <= limit; i++)
+//         if (i % 3 === 0 || i % 5 === 0)  // if i is divisible by 3 or 5
+//             sum += i;  // then add i to the sum
+
+//     return sum;
+// }
+
+// ================================================
+
+// Exercise 
+// Calculate grade
+
+// const array = [80, 80, 50];
+
+// // Average = 70
+
+// // 0-59: F
+// // 60-69: D
+// // 70-79: C
+// // 80-89: B
+// // 90-100: A
+
+// console.log(calculateGrade(array));
+
+// function calculateGrade(marks) {
+//     const average = calculateAverage(marks);
+    
+//     if (average < 60) return 'F';
+//     if (average < 70) return 'D';
+//     if (average < 80) return 'C';
+//     if (average < 90) return 'B';
+//     return 'A'
+
+// }
+
+// function calculateAverage(array) {
+//     let sum = 0;
+//     for (let value of array)
+//         sum += value;
+
+//     return sum / array.length;
+// }
+
+// ======================================
