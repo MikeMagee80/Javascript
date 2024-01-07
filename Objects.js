7c4592cadc2e3dcd1f94b30f45fc235a7e3bdf9e
MikeMagee80 <73151543+MikeMagee80@users.noreply.github.com>
2024-01-07 15:03:08 -0600

    Update Objects.js

--- Objects.js
+++ Objects.js
@@ -133,12 +133,12 @@
 
 // Cloning an Object
 
-const circle = {
-    radius: 1,
-    draw() {
-        console.log('draw');
-    }
-};
+// const circle = {
+//     radius: 1,
+//     draw() {
+//         console.log('draw');
+//     }
+// };
 
 // const another = {};
 
@@ -219,3 +219,75 @@ const circle = {
 // =================================================================
 
 // Exercise
+
+// street, city, zipCode
+// showAddress(address)
+
+// const address = {
+//     street: '5th',
+//     city: 'Austin',
+//     zipCode: 51104
+// };
+
+// function showAddress(address) {
+//     for (let key in address)
+//         console.log(key, address[key]);
+// }
+
+// showAddress(address);
+
+
+// ===============================
+
+// Factory Function
+
+// function createAddress(street, city, zipCode) {
+//     return {
+//         street,
+//         city,
+//         zipCode
+//     };
+// }
+
+// const address1 = createAddress('2nd', 'houston', 45022);
+// console.log(address1);
+
+// Constructor Function
+
+// let address = new Address('a', 'b', 'c');
+// console.log(address);
+
+// function Address(street, city, zipCode) {
+//     this.street = street;
+//     this.city = city;
+//     this.zipCode = zipCode;
+// }
+
+// ===========================
+
+// Object Equality
+
+// let address1 = new Address('a', 'b', 'c');
+// let address2 = new Address('a', 'b', 'c');
+// let address3 = address1;
+
+
+// console.log(areEqual(address1, address2));
+// console.log(areSame(address1, address2));
+// console.log(areSame(address1, address3));
+
+// function Address(street, city, zipCode) {
+//     this.street = street;
+//     this.city = city;
+//     this.zipCode = zipCode;
+// }
+
+// function areEqual(address1, address2) {
+//     return address1.street === address2.street && address1.city === address2.city && address1.zipCode === address2.zipCode;
+// }
+
+// function areSame(address1, address2) {
+//     return address1 === address2;  // if same, will return True
+// }
+
+// =======================================
